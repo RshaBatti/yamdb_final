673483f1123dc87d0c0e31b126c9b6c1406da5cf
Kirill <kporshnev@yandex.ru>
2021-05-28 14:28:51 +0300

    first push

--- titles/permissions.py
+++ titles/permissions.py
@@ -16,13 +16,13 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        elif request.user.is_authenticated:
+        elif request.user.is_authenticated is None:
             return bool(request.user.is_staff
                         or request.user.role == User.ADMIN)
 
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
+        if request.user.is_authenticated is None:
             return bool((request.user.role == User.ADMIN) or (
                 request.user.is_staff and request.user.is_superuser))
